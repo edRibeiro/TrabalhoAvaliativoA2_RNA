9afc463c97e43df9f4b9970b6a8cb1c5f55d7136
edRibeiro <eder.sonz@hotmail.com>
2017-02-17 20:57:55 -0200

    Editando o Perceptron para calcular em uma epoca.

--- src/trabalhoavaliativoa2_rna/Perceptron.java
+++ src/trabalhoavaliativoa2_rna/Perceptron.java
@@ -10,33 +10,59 @@ package trabalhoavaliativoa2_rna;
  * @author Tarcisio
  */
 public class Perceptron {
-    private int[][] x;
-    private double[] w;
-    private int[]d;
+    private int[][] xa;//entradas
+    private int[][] xb;//entradas
+    private double[] wa;//peso
+    private double[] wb;//peso
+    private double[] wc;//peso
+    private int[]da;//desejavel
+    private int[]db;//desejavel
+    private int[]dc;//desejavel
     private double a, e;
     private int max_int;
 
     public Perceptron(int[][] x, double[] w, int[] d, double a, double e, int max_int) {
-        this.x = x;
-        this.w = w;
-        this.d = d;
-        this.a = a;
-        this.e = e;
+        this.x = x;//entradas
+        this.w = w;//peso
+        this.d = d;//desejavel
+        this.a = a;//taxa da aprendixagem
+        this.e = e;//margem de erro
+        
         this.max_int = max_int;
     }
     
     public void treinar(){
-        double u=0;
+        
+        int[]ya=new int[4];//saida
+        int[]yb=new int[4];//saida
+        double ua=0;
+        double ub=0;
+        double uc=0;
         int epocas=0,y=0,ordem=0;
         boolean continua=true;
         while ((epocas<this.max_int)&&(continua)){
             continua=false;
             for (int i=0;i<this.x[0].length;i++){
                 //x1*w1+x2*w2+w0 
-                u=this.x[0][i]*this.w[0] + this.x[1][i]*this.w[1] + this.w[2];
+                ua=this.x[0][i]*this.w[0] + this.x[1][i]*this.w[1] + this.w[2];
                 
                 //Calculando a saída
-                if (u>=0){
+                if (ua>=0){
+                    ya[i]=1;
+                }else{
+                    ya[i]=0;
+                }
+                //Calculando a saída
+                if (ub>=0){
+                    yb[i]=1;
+                }else{
+                    yb[i]=0;
+                }
+                
+                uc=ya[i]*this.w[0] + yb[i]*this.w[1] + this.w[2];
+
+                //Calculando a saída
+                if (uc>=0){
                     y=1;
                 }else{
                     y=0;
