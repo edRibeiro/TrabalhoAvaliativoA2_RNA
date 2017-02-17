06b731c5a83cc06203b228fb3ba90faaab15d3b4
edRibeiro <eder.sonz@hotmail.com>
2017-02-17 19:34:52 -0200

    Primeio commit do projeto recem criado.

---
+++ src/trabalhoavaliativoa2_rna/TrabalhoAvaliativoA2_RNA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhoavaliativoa2_rna;
+
+/**
+ *
+ * @author lab1
+ */
+public class TrabalhoAvaliativoA2_RNA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
